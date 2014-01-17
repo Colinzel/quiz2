ff23e1a4be2426d3bcb3d7c5d7a5171144922b17
Colin Zelin <Colin@Colin-Zelins-MacBook-Pro.local>
2014-01-17 10:04:04 -0500

    Initial Commit

---
+++ quiz2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  quiz2
+//
+//  Created by Colin Zelin on 1/17/14.
+//  Copyright (c) 2014 Colin Zelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ quiz2Tests/quiz2Tests.m
@@ -0,0 +1,34 @@
+//
+//  quiz2Tests.m
+//  quiz2Tests
+//
+//  Created by Colin Zelin on 1/17/14.
+//  Copyright (c) 2014 Colin Zelin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface quiz2Tests : XCTestCase
+
+@end
+
+@implementation quiz2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
